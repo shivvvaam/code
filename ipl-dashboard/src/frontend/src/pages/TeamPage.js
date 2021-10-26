7209777b1af8870b5ae8ecd0084f30a56efc6c91
shivam <singh.shivam97@yahoo.com>
2021-10-27 01:42:37 +0530

    Styled the team page

--- ipl-dashboard/src/frontend/src/pages/TeamPage.js
+++ ipl-dashboard/src/frontend/src/pages/TeamPage.js
@@ -1,35 +1,53 @@
-import {React,useEffect,useState} from "react";
-import {useParams} from 'react-router-dom';
+import { React, useEffect, useState } from "react";
+import { useParams } from 'react-router-dom';
 import { MatchDetailCard } from "../components/MatchDetailCard";
 import { MatchSmallCard } from "../components/MatchSmallCard";
 
-export const TeamPage=()=> {
+import { PieChart } from 'react-minimal-pie-chart';
 
-    const [team,setTeam]=useState({matches : []});
-    const {teamName} = useParams();
-    useEffect(
-        ()=>{
-            const fetchMatches = async ()=>{
-                const response=await fetch(`http://localhost:8080/team/${teamName}`);
-                const data=await response.json();
-                setTeam(data);
-            };
-            fetchMatches();
-        },[teamName]
-    );
+import './TeamPage.css';
 
-    if(!team || !team.teamName){
-      return <h1>Team not found</h1>
-    }
+export const TeamPage = () => {
+
+  const [team, setTeam] = useState({ matches: [] });
+  const { teamName } = useParams();
+  useEffect(
+    () => {
+      const fetchMatches = async () => {
+        const response = await fetch(`http://localhost:8080/team/${teamName}`);
+        const data = await response.json();
+        setTeam(data);
+      };
+      fetchMatches();
+    }, [teamName]
+  );
+
+  if (!team || !team.teamName) {
+    return <h1>Team not found</h1>
+  }
 
   return (
     <div className="TeamPage">
-    <h1> {team.teamName} </h1>
-
-
-    <MatchDetailCard teamName={team.teamName} match={team.matches[0]}/>
-    {team.matches.slice(1).map(match => <MatchSmallCard match={match} teamName={team.teamName}/>)}
-
+      <div className="team-name-section">
+        <h1 className="team-name"> {team.teamName} </h1>
+      </div>
+      <div className="win-loss-section">
+        Wins / Losses
+        <PieChart
+  data={[
+    { title: 'Losses', value: team.totalMatches - team.totalWins, color: '#a34d5d' },
+    { title: 'Wins', value: team.totalWins, color: '#4da375' }
+  ]}
+/>
+      </div>
+      <div className="match-detail-section">
+        <h3>Latest Matches</h3>
+        <MatchDetailCard teamName={team.teamName} match={team.matches[0]} />
+      </div>
+      {team.matches.slice(1).map(match => <MatchSmallCard match={match} teamName={team.teamName} />)}
+      <div className="more-link">
+        <a href="#">More ></a>
+      </div>
 
     </div>
   );
